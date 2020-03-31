abfc9d830eabb82806e819ae56317e355846750a
Miao Nan Lin <miaonanlin@gmail.com>
2020-03-31 22:14:15 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,9 @@ var playlist={[name]: 'title'}
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
+}
 
+function removeFromPlaylist(playlist, artistName){
+  return delete playlist.artistName
+  
 }
